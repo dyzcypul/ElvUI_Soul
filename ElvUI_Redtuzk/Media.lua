c5648fff6c761fc3ae105ef9d6019c2731677dbe
Aldarana <micheller.baran96@gmail.com>
2018-04-29 14:35:50 +0000

    Removed the SharedMediaAdditionalFonts dependency. We can just use the SharedMedia library which is included with ElvUI.

--- ElvUI_Redtuzk/Media.lua
+++ ElvUI_Redtuzk/Media.lua
@@ -1,5 +1,8 @@
 local RUI = select(2, ...):unpack()
 
+local LSM = LibStub("LibSharedMedia-3.0")
+
 function RUI:RegisterMedia()
-	SharedMediaAdditionalFonts:Register("font", "Century Gothic Bold", [[Interface\Addons\ElvUI_Redtuzk\Media\GOTHICB.TTF]])
+	if LSM == nil then return end
+	LSM:Register("font", "Century Gothic Bold", [[Interface\Addons\ElvUI_Redtuzk\Media\GOTHICB.TTF]])
 end
